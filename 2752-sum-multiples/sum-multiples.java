7508b62ee9390bedfbd34a035bf4aa50f6bba93b
Rachabattuni Sai Sindhu <rsindhu0804@gmail.com>
2026-07-02 11:20:48 +0530

    Time: 3 ms (95.36%) | Memory: 42.8 MB (22.80%) - LeetSync

--- 2752-sum-multiples/sum-multiples.java
+++ 2752-sum-multiples/sum-multiples.java
@@ -2,12 +2,12 @@ class Solution {
     public int sumOfMultiples(int n) {
        int sum = 0;
        int i = 1;
-        while(i <= n) {
+        do {
             if(i % 3 == 0 || i % 5 == 0 || i % 7 == 0) {
                 sum = sum + i;
             }
             i++;
-        }
+        }while(i <= n);
         return sum;
     }
 }
