98152598529ad7198240e98285798e453c609678
Rachabattuni Sai Sindhu <rsindhu0804@gmail.com>
2026-07-07 09:34:06 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (38.58%) - LeetSync

--- 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.java
+++ 1965-sum-of-digits-in-base-k/sum-of-digits-in-base-k.java
@@ -1,10 +1,12 @@
 class Solution {
     public int sumBase(int n, int k) {
-        int sum = 0;
-        while(n > 0) {
-            sum += n % k;
-            n /= k;
-        }
-        return sum;
+    int ori = n;
+    int sum = 0;
+    while(ori>0){
+        int digit = ori % k;
+        ori = ori / k;
+        sum = sum + digit;
+    }
+    return sum;
     }
 }
