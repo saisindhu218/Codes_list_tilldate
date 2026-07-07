984032439fa456f1bf086b44f05e8d622c9c98e3
Rachabattuni Sai Sindhu <rsindhu0804@gmail.com>
2026-07-07 11:42:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.5 MB (12.90%) - LeetSync

---
+++ 2630-alternating-digit-sum/alternating-digit-sum.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int alternateDigitSum(int n) {
+       int temp = n;
+       int count = 0;
+       int sum = 0;
+       while(n>0){
+        int digit = n % 10;
+        n = n / 10;
+        count++;
+       } 
+       while(temp>0){
+        int digit = temp % 10;
+        temp = temp / 10;
+        count--;
+        if(count % 2 == 0){
+            sum = sum + digit;
+        }else{
+            sum = sum - digit;
+        }
+       }
+     return sum;
+    }
+}
