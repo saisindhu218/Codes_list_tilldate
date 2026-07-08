789eda95c538f7114958e698dd328115af03aa2c
Rachabattuni Sai Sindhu <rsindhu0804@gmail.com>
2026-07-08 15:02:16 +0530

    Time: 0 ms (100.00%) | Memory: 42.1 MB (58.46%) - LeetSync

--- 2083-three-divisors/three-divisors.java
+++ 2083-three-divisors/three-divisors.java
@@ -3,18 +3,23 @@ class Solution {
         int check = 1;
         int count = 0;
         if ( n == 0 ){
-            return false;
+            return false;      // if number is 0 
         }
-        for ( int i = 1; i <= n; i++){
-            if ( i *i == n) {
-                check = i;
-            }
+
+        for ( int i = 1; i <= Math.sqrt(n); i++){
+            if ( i *i == n) {            // this loop is to identify square root of num
+                check = i;              // i.e if num = 25      5 * 5 = 25
+            }                            // so 5 is squareroot of 25
+            if ( i * i > n ){
+                return false;
+            } 
         }
+
         for ( int j = 1; j <= check; j++ ){
             if ( check % j == 0 ){
-                count++;
-            }
-        }
+                count++;                        // this loop is to check if the root is 
+            }                                   // prime or not as prime has 2 divisors but its
+        }                                       // square has 3 divisors
 
         if ( count == 2 ){
             return true;
