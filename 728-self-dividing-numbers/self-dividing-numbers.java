b3db01130da5643f60df4153c13289e910fcddb4
Rachabattuni Sai Sindhu <rsindhu0804@gmail.com>
2026-07-08 10:08:13 +0530

    Time: 2 ms (98.82%) | Memory: 42.4 MB (91.02%) - LeetSync

--- 728-self-dividing-numbers/self-dividing-numbers.java
+++ 728-self-dividing-numbers/self-dividing-numbers.java
@@ -1,21 +1,21 @@
 class Solution {
     public List<Integer> selfDividingNumbers(int left, int right) {
-        List<Integer> answer= new ArrayList<>();
-        for(int i = left; i<=right; i++){
-            int temp = i;
-            boolean isselfdiving = true;
-            while(temp>0){
-                int digit = temp % 10;
-                if(digit==0 || i % digit != 0){
-                    isselfdiving = false;
-                    break;
-                }
-                temp = temp / 10;
+    List<Integer> answer= new ArrayList<>();
+    for(int i=left; i<=right;i++){
+        int temp = i;
+        boolean isvalid= true;
+        while(temp>0){
+            int digit = temp % 10;
+            if(digit == 0 || i % digit != 0){
+                isvalid =false;
+                break;
             }
-            if(isselfdiving){
-             answer.add(i);
-            }
+            temp = temp / 10;
         }
-        return answer;
+        if(isvalid){
+        answer.add(i);
+        }
+    }
+    return answer;
     }
 }
