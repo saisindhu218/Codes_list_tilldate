6455d8f84d016258853ec3be50c81a8c4efdca80
Rachabattuni Sai Sindhu <rsindhu0804@gmail.com>
2026-07-10 09:26:03 +0530

    Time: 8 ms (91.92%) | Memory: 45.9 MB (65.64%) - LeetSync

--- 326-power-of-three/power-of-three.java
+++ 326-power-of-three/power-of-three.java
@@ -4,25 +4,24 @@ class Solution {
         if ( n <= 0 ){
             return false;
         }
-        if ( n == 1 ){
+        if ( n == 1){
             return true;
         }
 
-        if( n % 3 == 0 ){
-            while ( check > 1 ){
-               check = check / 3;
-               if(check == 1){
+        if ( n % 3 == 0){
+            while ( check > 1){
+            check = check / 3;
+            if ( check == 1 ){
                 return true;
-               }
-               if (check % 3 != 0){
-                return false;
-               }
             }
-        return true;
+            if ( check % 3 != 0){
+               return false;
+            }
+            }
+            return true;
         }else{
             return false;
         }
-
 
     }
 }
