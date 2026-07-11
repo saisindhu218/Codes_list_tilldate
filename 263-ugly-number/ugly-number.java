216668248845900728a6c1dbb0fe0159ed927a20
Rachabattuni Sai Sindhu <rsindhu0804@gmail.com>
2026-07-11 10:13:07 +0530

    Time: 1 ms (99.01%) | Memory: 42.6 MB (32.68%) - LeetSync

---
+++ 263-ugly-number/ugly-number.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean isUgly(int n) {
+        int check = n;
+        if ( n == 1){
+            return true;
+        }
+        if ( n <= 0 ){
+            return false;
+        }
+
+        while ( check > 1 ){
+        if ( check % 2 == 0){
+            check = check / 2;
+            continue;
+        }else 
+        if ( check % 3 == 0){
+           check =  check / 3;
+           continue;
+        }else
+        if( check % 5 == 0){
+            check = check / 5;
+            continue;
+        }else{
+            return false;
+        } 
+        }
+        return true;
+    }
+}
