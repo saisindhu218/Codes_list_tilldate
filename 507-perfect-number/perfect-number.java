a7d31488912fce828b743bf908632e4250fc19c6
Rachabattuni Sai Sindhu <rsindhu0804@gmail.com>
2026-07-14 12:19:51 +0530

    Time: 2 ms (83.41%) | Memory: 42 MB (63.41%) - LeetSync

--- 507-perfect-number/perfect-number.java
+++ 507-perfect-number/perfect-number.java
@@ -1,15 +1,20 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum = 0;
+        int sum = 1;
 
         if ( num == 1 ){
             return false;
         }
 
-        for ( int i = 1; i <= num/2; i++){
+        for ( int i = 2; i <= Math.sqrt(num); i++){
             if ( num % i == 0 ){
-                sum = sum + i;
-            }
+                if ( num / i == i ){
+                    sum = sum + i;
+                }else{
+                int ans = i + (num / i);
+                sum = sum + ans;
+            } }
+
         }
         if ( sum == num ){
             return true;
